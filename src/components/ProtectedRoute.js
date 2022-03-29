f7f1d7e256bf0f0f4e6aeaf26317376ee381b046
Grégory D'Angelo <dangelo.gregory@gmail.com>
2022-03-29 17:38:20 +0200

    refact: Update nhost/react lib to use useAuthenticationStatus

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,10 +1,10 @@
-import { useNhostAuth } from '@nhost/react';
+import { useAuthenticationStatus } from '@nhost/react';
 import React from 'react';
 import { Navigate, useLocation } from 'react-router';
 import Spinner from './Spinner';
 
 const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated, isLoading } = useNhostAuth();
+  const { isAuthenticated, isLoading } = useAuthenticationStatus();
   const location = useLocation();
 
   if (isLoading) {
