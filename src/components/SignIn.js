7fa1c8bf0800950d89cb16f8cb8595419167e0a9
Grégory D'Angelo <dangelo.gregory@gmail.com>
2022-04-08 10:31:40 +0200

    refact: Disable form if account not verified

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -26,6 +26,8 @@ const SignIn = () => {
     return <Navigate to="/" replace={true} />;
   }
 
+  const disableForm = isLoading || needsEmailVerification;
+
   return (
     <div className="w-full max-w-lg">
       <div className="sm:rounded-xl sm:shadow-md sm:border border-opacity-50 sm:bg-white px-4 sm:px-8 py-12 flex flex-col items-center">
@@ -38,7 +40,7 @@ const SignIn = () => {
               label="Email address"
               value={email}
               onChange={e => setEmail(e.target.value)}
-              disabled={isLoading}
+              disabled={disableForm}
               required
             />
             <Input
@@ -46,14 +48,14 @@ const SignIn = () => {
               label="Password"
               value={password}
               onChange={e => setPassword(e.target.value)}
-              disabled={isLoading}
+              disabled={disableForm}
               required
             />
           </div>
 
           <button
             type="submit"
-            disabled={isLoading}
+            disabled={disableForm}
             className="mt-6 w-full font-medium inline-flex justify-center items-center rounded-md p-3 text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed  disabled:hover:bg-blue-600 disabled:hover:border-bg-600 transition-colors"
           >
             {isLoading ? <Spinner size="sm" /> : 'Sign in'}
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -34,6 +34,8 @@ const SignUp = () => {
     return <Navigate to="/" replace={true} />;
   }
 
+  const disableForm = isLoading || needsEmailVerification;
+
   return (
     <div className="w-full max-w-lg">
       <div className="sm:rounded-xl sm:shadow-md sm:border border-opacity-50 sm:bg-white px-4 sm:px-8 py-12 flex flex-col items-center">
@@ -46,14 +48,14 @@ const SignUp = () => {
                 label="First name"
                 value={firstName}
                 onChange={e => setFirstName(e.target.value)}
-                disabled={isLoading}
+                disabled={disableForm}
                 required
               />
               <Input
                 label="Last name"
                 value={lastName}
                 onChange={e => setLastName(e.target.value)}
-                disabled={isLoading}
+                disabled={disableForm}
                 required
               />
             </div>
@@ -62,7 +64,7 @@ const SignUp = () => {
               label="Email address"
               value={email}
               onChange={e => setEmail(e.target.value)}
-              disabled={isLoading}
+              disabled={disableForm}
               required
             />
             <Input
@@ -70,14 +72,14 @@ const SignUp = () => {
               label="Create password"
               value={password}
               onChange={e => setPassword(e.target.value)}
-              disabled={isLoading}
+              disabled={disableForm}
               required
             />
           </div>
 
           <button
             type="submit"
-            disabled={isLoading}
+            disabled={disableForm}
             className="mt-6 w-full font-medium inline-flex justify-center items-center rounded-md p-3 text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed  disabled:hover:bg-blue-600 disabled:hover:border-bg-600 transition-colors"
           >
             {isLoading ? <Spinner size="sm" /> : 'Create account'}
