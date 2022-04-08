5a59d58af9dc33b8ebaac104424c0da0a4596ae6
Grégory D'Angelo <dangelo.gregory@gmail.com>
2022-04-08 11:11:21 +0200

    refact: Show verification message only if account not verified

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -39,50 +39,56 @@ const SignIn = () => {
           />
         </div>
 
-        <form onSubmit={handleOnSubmit} className="w-full">
-          <div className="mt-12 w-full flex flex-col items-center space-y-6">
-            <Input
-              type="email"
-              label="Email address"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              disabled={disableForm}
-              required
-            />
-            <Input
-              type="password"
-              label="Password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              disabled={disableForm}
-              required
-            />
-          </div>
+        {needsEmailVerification ? (
+          <p className="mt-12 text-center">
+            Please check your mailbox and follow the verification link to verify
+            your email.
+          </p>
+        ) : (
+          <>
+            <form onSubmit={handleOnSubmit} className="w-full">
+              <div className="mt-12 w-full flex flex-col items-center space-y-6">
+                <Input
+                  type="email"
+                  label="Email address"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                  disabled={disableForm}
+                  required
+                />
+                <Input
+                  type="password"
+                  label="Password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  disabled={disableForm}
+                  required
+                />
+              </div>
 
-          <button
-            type="submit"
-            disabled={disableForm}
-            className="mt-6 w-full font-medium inline-flex justify-center items-center rounded-md p-3 text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed  disabled:hover:bg-blue-600 disabled:hover:border-bg-600 transition-colors"
-          >
-            {isLoading ? <Spinner size="sm" /> : 'Sign in'}
-          </button>
+              <button
+                type="submit"
+                disabled={disableForm}
+                className="mt-6 w-full font-medium inline-flex justify-center items-center rounded-md p-3 text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed  disabled:hover:bg-blue-600 disabled:hover:border-bg-600 transition-colors"
+              >
+                {isLoading ? <Spinner size="sm" /> : 'Sign in'}
+              </button>
 
-          {isError ? (
-            <p className="mt-4 text-red-500 text-center">{error?.message}</p>
-          ) : needsEmailVerification ? (
-            <p className="mt-4 text-orange-500 text-center">
-              Please check your mailbox and follow the verification link to
-              verify your email
-            </p>
-          ) : null}
-        </form>
+              {isError ? (
+                <p className="mt-4 text-red-500 text-center">
+                  {error?.message}
+                </p>
+              ) : null}
+            </form>
 
-        <Link
-          to="/reset-password"
-          className="mt-4 text-blue-600 hover:text-blue-500 hover:underline hover:underline-offset-1 transition"
-        >
-          Forgot your password?
-        </Link>
+            <Link
+              to="/reset-password"
+              className="mt-4 text-blue-600 hover:text-blue-500 hover:underline hover:underline-offset-1 transition"
+            >
+              Forgot your password?
+            </Link>
+          </>
+        )}
       </div>
 
       <p className="sm:mt-8 text-gray-500 text-center">
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -47,59 +47,61 @@ const SignUp = () => {
           />
         </div>
 
-        <form onSubmit={handleOnSubmit} className="w-full">
-          <div className="mt-12 flex flex-col items-center space-y-6">
-            <div className="w-full flex gap-6">
+        {needsEmailVerification ? (
+          <p className="mt-12 text-center">
+            Please check your mailbox and follow the verification link to verify
+            your email.
+          </p>
+        ) : (
+          <form onSubmit={handleOnSubmit} className="w-full">
+            <div className="mt-12 flex flex-col items-center space-y-6">
+              <div className="w-full flex gap-6">
+                <Input
+                  label="First name"
+                  value={firstName}
+                  onChange={e => setFirstName(e.target.value)}
+                  disabled={disableForm}
+                  required
+                />
+                <Input
+                  label="Last name"
+                  value={lastName}
+                  onChange={e => setLastName(e.target.value)}
+                  disabled={disableForm}
+                  required
+                />
+              </div>
               <Input
-                label="First name"
-                value={firstName}
-                onChange={e => setFirstName(e.target.value)}
+                type="email"
+                label="Email address"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
                 disabled={disableForm}
                 required
               />
               <Input
-                label="Last name"
-                value={lastName}
-                onChange={e => setLastName(e.target.value)}
+                type="password"
+                label="Create password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
                 disabled={disableForm}
                 required
               />
             </div>
-            <Input
-              type="email"
-              label="Email address"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              disabled={disableForm}
-              required
-            />
-            <Input
-              type="password"
-              label="Create password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              disabled={disableForm}
-              required
-            />
-          </div>
 
-          <button
-            type="submit"
-            disabled={disableForm}
-            className="mt-6 w-full font-medium inline-flex justify-center items-center rounded-md p-3 text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed  disabled:hover:bg-blue-600 disabled:hover:border-bg-600 transition-colors"
-          >
-            {isLoading ? <Spinner size="sm" /> : 'Create account'}
-          </button>
+            <button
+              type="submit"
+              disabled={disableForm}
+              className="mt-6 w-full font-medium inline-flex justify-center items-center rounded-md p-3 text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed  disabled:hover:bg-blue-600 disabled:hover:border-bg-600 transition-colors"
+            >
+              {isLoading ? <Spinner size="sm" /> : 'Create account'}
+            </button>
 
-          {isError ? (
-            <p className="mt-4 text-red-500 text-center">{error?.message}</p>
-          ) : needsEmailVerification ? (
-            <p className="mt-4 text-orange-500 text-center">
-              Please check your mailbox and follow the verification link to
-              verify your email
-            </p>
-          ) : null}
-        </form>
+            {isError ? (
+              <p className="mt-4 text-red-500 text-center">{error?.message}</p>
+            ) : null}
+          </form>
+        )}
       </div>
 
       <p className="sm:mt-8 text-gray-500 text-center">
